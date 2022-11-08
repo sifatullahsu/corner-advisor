64a1a4ddead6d0f40d82177732a153721336065f
sifatullahsu <personal.sifat@gmail.com>
2022-11-08 06:46:28 +0600

    google signin button

---
+++ src/components/GoogleSignin.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContextComp';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const GoogleSignIn = ({ from }) => {
+  const { userSocialLogin } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleGoogleSignIn = () => {
+    userSocialLogin('google')
+      .then(res => {
+        toast.success('Successfully logged in!!');
+        navigate(from);
+      })
+      .catch(err => {
+        toast.error('Something is wrong!!');
+      })
+  }
+
+  return (
+    <>
+      <button onClick={handleGoogleSignIn} className='btn'>Google</button>
+    </>
+  );
+};
+
+export default GoogleSignIn;
